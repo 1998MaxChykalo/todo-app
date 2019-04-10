0ecdb9a17710b211d90fb9ce9ef0109c6995f4c8
1998MaxChykalo <maxymchykalo@gmail.com>
2019-04-10 06:37:28 +0300

    Updates

---
+++ precache-manifest.34abb25bcdedaf0eb9205d9b595ae003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  },
+  {
+    "revision": "f69dfcc0a1a8b32b2a35",
+    "url": "/todo-app/static/js/main.20ab7d6d.chunk.js"
+  },
+  {
+    "revision": "598b068609625b89662e",
+    "url": "/todo-app/static/js/2.47fb247a.chunk.js"
+  },
+  {
+    "revision": "f69dfcc0a1a8b32b2a35",
+    "url": "/todo-app/static/css/main.9b24e412.chunk.css"
+  },
+  {
+    "revision": "598b068609625b89662e",
+    "url": "/todo-app/static/css/2.460e7306.chunk.css"
+  },
+  {
+    "revision": "77c4aa2b6a262a4f1f85d0ff28d0b7f9",
+    "url": "/todo-app/index.html"
+  }
+];
