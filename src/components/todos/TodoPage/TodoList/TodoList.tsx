bb7789b0d88d93b855f6dc1ecbe29353428e886d
1998MaxChykalo <maxymchykalo@gmail.com>
2019-04-11 08:41:46 +0300

    Add ability to sort by date, name and estimated time. fix some styles

--- src/components/todos/TodoPage/TodoList/TodoList.tsx
+++ src/components/todos/TodoPage/TodoList/TodoList.tsx
@@ -2,11 +2,11 @@ import { connect } from 'react-redux';
 
 import { AppState } from './../../../../store';
 
-import { deleteTodo, updateTodo } from '../../../../actions/todo/actions';
+import { deleteTodo, updateTodo, sortTodos } from '../../../../actions/todo/actions';
 
 import { getActiveTodosByTerm } from '../../../../selectors/todo-selectors';
 
-import { ITodo } from '../../../../reducers/todoReducer';
+import { ITodo, ISortableTodoColumns } from '../../../../reducers/todoReducer';
 
 import { TodoListComponent } from './TodoListComponent';
 import { UpdateTodoModel } from '../../../../dto/update-todo.model';
@@ -17,12 +17,13 @@ export interface IStateProps {
 export interface IDispatchProps {
   deleteTodo(id: number): void;
   updateTodo(updateTodoModel: UpdateTodoModel): void;
+  sortTodos(column: keyof ISortableTodoColumns): void;
 }
 
 const mapStateToProps = (state: AppState): IStateProps => ({
   todos: getActiveTodosByTerm(state)
 });
 
-const mapDispatchToProps = { deleteTodo, updateTodo }
+const mapDispatchToProps = { deleteTodo, updateTodo, sortTodos }
 
 export default connect<IStateProps, IDispatchProps, null, AppState>(mapStateToProps, mapDispatchToProps)(TodoListComponent);
--- src/components/todos/TodoPage/TodoList/TodoListComponent.tsx
+++ src/components/todos/TodoPage/TodoList/TodoListComponent.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { ITodo, TodoStatus } from './../../../../reducers/todoReducer';
 
-import { Table, Icon, Tag } from 'antd';
+import { Table, Icon, Tag, Form, Radio } from 'antd';
 
 import { IStateProps, IDispatchProps } from './TodoList';
 
@@ -10,16 +10,16 @@ import { useTranslation } from 'react-i18next';
 
 import './TodoItem/TodoItem.scss';
 import { formatTime } from '../../../../selectors/todo-selectors';
-
+import FormItem from 'antd/lib/form/FormItem';
+import {ColumnProps} from 'antd/lib/table';
 type Props = IStateProps & IDispatchProps;
 
-export const TodoListComponent: React.FC<Props> = ({ todos, deleteTodo, updateTodo }) => {
+export const TodoListComponent: React.FC<Props> = ({ todos, deleteTodo, updateTodo, sortTodos }) => {
 
   const { t, i18n } = useTranslation();
   React.useEffect(() => {
 
     const isInProgress = (todo: ITodo) => todo.status === TodoStatus['In Progress'];
-    const exist = <T, K extends keyof T>(prop: K, obj: T) => (obj[prop]) !== null && (obj[prop]) !== undefined;
     const interval = setInterval(() => todos.filter(isInProgress).map(todo => {
 
       if (todo.timeTillEnd) {
@@ -29,17 +29,20 @@ export const TodoListComponent: React.FC<Props> = ({ todos, deleteTodo, updateTo
 
     return () => clearInterval(interval);
   });
-
   const columns = [
     {
+      align: 'center' as 'center',
       title: t('text'),
       dataIndex: 'text',
       key: 'text',
+      // defaultSortOrder: 'descend',
+      sorter: (a: ITodo, b: ITodo) => a.text.length - b.text.length,
       render: (text: string) => (
         <span className='todo__item__text'>{text}</span>
       )
     },
     {
+      align: 'center' as 'center',
       title: 'status',
       dataIndex: 'status',
       key: 'status',
@@ -48,42 +51,49 @@ export const TodoListComponent: React.FC<Props> = ({ todos, deleteTodo, updateTo
       )
     },
     {
+      align: 'center' as 'center',
       title: 'createdAt',
       dataIndex: 'createdAt',
       key: 'createdAt',
+      // defaultSortOrder: 'descend',
+      sorter: (a: ITodo, b: ITodo) =>  a.createdAt > b.createdAt ? 1 : -1,
       render: (date: Date) => <span>{new Date(date).toLocaleDateString()}</span>
     },
     {
+      align: 'center' as 'center',
       title: 'Tags',
       dataIndex: "tags",
       key: 'tags',
       render: (tags: string[]) => (
         <span>
-          {tags && tags.map(tag => <Tag color="blue" key={tag}>#{tag}</Tag>)}
+          {tags.length > 0 ? tags.map(tag => <Tag color="blue" key={tag}>#{tag}</Tag>) : '-'}
         </span>
       )
     },
     {
+      align: 'center' as 'center',
       title: 'Estimated Time',
       dataIndex: "estimatedTime",
       key: 'estimatedTime',
       render: (estimatedTime: number) => (
         <span>
-          {estimatedTime && formatTime(estimatedTime)}
+          {estimatedTime > 0 ? formatTime(estimatedTime) : '-'}
         </span>
       )
     },
     {
+      align: 'center' as 'center',
       title: 'Left',
       dataIndex: "timeTillEnd",
       key: 'timeTillEnd',
       render: (timeTillEnd: number) => (
         <span>
-          {timeTillEnd && formatTime(timeTillEnd)}
+          {timeTillEnd > 0 ? formatTime(timeTillEnd) : '-'}
         </span>
       )
     },
     {
+      align: 'center' as 'center',
       title: 'Actions',
       key: 'actions',
       render: (record: ITodo) => {
@@ -104,5 +114,19 @@ export const TodoListComponent: React.FC<Props> = ({ todos, deleteTodo, updateTo
       }
     }
   ];
-  return <Table rowClassName={(record: ITodo) => record.status === TodoStatus.Completed ? 'todo__item--completed' : ''} columns={columns} dataSource={todos} pagination={false} />
+  
+  return (
+    <>
+    <Form layout="inline">
+        <FormItem label="Sort By">
+      <Radio.Group size="default" onChange={(e) => sortTodos(e.target.value)}>
+        <Radio.Button value="createdAt">Date</Radio.Button>
+        <Radio.Button value="text">Name</Radio.Button>
+        <Radio.Button value="estimatedTime">Estimated Time</Radio.Button>
+      </Radio.Group>
+    </FormItem>
+  </Form>
+    <Table rowClassName={(record: ITodo) => record.status === TodoStatus.Completed ? 'todo__item--completed' : ''} columns={columns} dataSource={todos} pagination={false} />
+    </>
+  )
 };
