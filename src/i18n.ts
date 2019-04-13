d9c0eff607a65d96d4e8cb5bd0911a099e7d6f5f
1998MaxChykalo <maxymchykalo@gmail.com>
2019-04-13 11:25:25 +0300

    Add translations for en and ua languages

--- src/i18n.ts
+++ src/i18n.ts
@@ -6,7 +6,56 @@ import { initReactI18next } from "react-i18next";
 const resources = {
   en: {
     translation: {
-      "text": "Текст"
+      sortBy: 'Sort By',
+      date: 'Date',
+      text: 'Text',
+      estimatedTime: 'Estimated Time',
+      addTag: 'Add Tag',
+      addToDo: 'Add ToDo',
+      selectTime: 'Chose Time',
+      todo: {
+        text: "Text",
+        status: "Status",
+        createdAt: "Created At",
+        tags: "Tags",
+        estimatedTime: "Estimated Time",
+        left: "Left",
+        actions: "Actions",
+        statuses: {
+          "All": "All",
+          "Active": "Active",
+          "Completed": "Completed",
+          "In Progress": "In Progress",
+          "Paused": "In Pause",    
+        } 
+      },
+    }
+  },
+  ua: {
+    translation: {
+      sortBy: 'Сортувати по',
+      date: 'Даті',
+      text: 'Тексту',
+      estimatedTime: 'Запланованому часу',
+      addTag: 'Додати тег',
+      addToDo: 'Додати завдання',
+      selectTime: 'Виберіть час',
+      todo: {
+        text: "Текст",
+        status: "Статус",
+        createdAt: "Створено в",
+        tags: "Теги",
+        estimatedTime: "Заплановано",
+        left: "Залишилось",
+        actions: "Дії",
+        statuses: {
+          "All": "Всі",
+          "Active": "Активні",
+          "Completed": "Завершені",
+          "In Progress": "В прогресі",
+          "Paused": "На паузі",    
+        } 
+      },
     }
   }
 };
@@ -15,7 +64,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en",
+    lng: "ua",
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
